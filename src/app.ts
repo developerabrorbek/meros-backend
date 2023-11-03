cce834357ed922ff40a4eea9f6eafa0efee09409
Abrorbek abdulkhamidov <abdulkhamidovabrorbek@gmail.com>
2023-11-03 10:52:49 +0500

    configs enabled

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { databaseConfig } from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+    })
+  ],
 })
 export class AppModule {}
